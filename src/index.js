3533aa06a91af9a3d057ed69acb4a8179bca4819
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-08-12 17:43:48 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { DataContextProvider } from './contexts/DataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <DataContextProvider>
     <App />
+    </DataContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
